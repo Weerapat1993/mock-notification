d994d30053b1f2e75209a23daad8d9051844b2b2
Weerapat-Dotography <weerapat.t@dotography.com>
2017-05-22 18:15:08 +0700

    Update API & Fix Bug

--- app/API/AuthAPI.js
+++ app/API/AuthAPI.js
@@ -1,31 +1,47 @@
 var AuthAPI = module.exports = {};
+// var data = [
+//   {
+//     id: 1,
+//     name: 'Admin Example',
+//     email: 'admin@example.com',
+//     password: 'qwerty',
+//   }
+// ]
 
 AuthAPI.login = function(req, res, next) {
   var user = null;
+  var msg = null;
   var data = {
     id: 1,
     name: 'Admin Example',
     email: 'admin@example.com',
     password: 'qwerty',
   }
+  var msg = null;
 
-  if(req.body.email.toLowerCase() == data.email.toLowerCase() && req.body.password == data.password) {
-    user = data,
+  if(req.body.email.toLowerCase() == data.email.toLowerCase()) {
+    if(req.body.password.toLowerCase() != data.password.toLowerCase()){
+      msg = 'Password Incorrect'
+    } else {
+      msg = 'Login Successful'
+      user = data;
+    }
     res.send({
       data: user,
       code: 200,
       status: 'OK',
       error: {
-        message: 'Login Successful'
+        message: msg
       }
     });
   } else {
+    msg = 'User is not found'
     res.send({
       data: user,
       code: 500,
       status: 'Error',
       error: {
-        message: 'User is not found'
+        message: msg
       }  
     });
   }
